71f50a77a584d8f8501bfd358a440afbe3f9edb3
Titi <tristanfio05@gmail.com>
2023-12-13 10:31:59 +0100

    Creation CRUD & DB with Postman

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    GamesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/games/games.service.ts
@@ -0,0 +1,47 @@
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateGameDto } from './dto/create-game.dto';
+import { UpdateGameDto } from './dto/update-game.dto';
+import { Game } from './entities/game.entity';
+
+@Injectable()
+export class GamesService {
+  constructor(@InjectRepository(Game) private data: Repository<Game>) { }
+
+  async create(dto: CreateGameDto): Promise<Game> {
+    try {
+      return await this.data.save(dto);
+    } catch (e) {
+      throw new ConflictException();
+    }
+  }
+
+  findAll(): Promise<Game[]> {
+    return this.data.find();
+  }
+
+  async findOne(id: number): Promise<Game> {
+    const found = await this.data.findOneBy({ id });
+    if (!found)
+      throw new NotFoundException();
+    return found;
+  }
+
+  async update(id: number, dto: UpdateGameDto): Promise<Game> {
+    try {
+      let done = await this.data.update(id, dto);
+      if (done.affected != 1)
+        throw new NotFoundException();
+    } catch (e) {
+      throw e instanceof NotFoundException ? e : new ConflictException();
+    }
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    let done = await this.data.delete(id);
+    if (done.affected != 1)
+      throw new NotFoundException();
+  }
+}
